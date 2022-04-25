c6c66c64d7253911e8498175c928878782adf436
play.js <git@gituser.com>
2022-04-25 12:50:45 -0400

    more dependencies and better control flow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require('axios'); 
 const url = "https://projecteuler.net/problem="; 
+const dom = require('jsdom'); 
+const { JSDOM } = dom; 
 
 async function main() {
      await getDailyProblem(); 
@@ -11,11 +13,15 @@ async function getDailyProblem() {
    let number = Math.floor(Math.random() * 700); 
    
 // retrieve problem from ProjectEuler
-     console.log(`${url}${number}`);
-     const p = await axios.get(`${url}${number}`).then((problem) => { 
-       console.log(problem); 
-     }); 
-console.log(p)
+     console.log(`(@___@)/kimikoProblemBotv1:
+[RunningTask] => Fetching Project Euler Problem: # ${number}`);
+   const p = await axios.get(`${url}${number}`).then((problem) => { 
+       //console.log(problem.data); 
+       const dom = new JSDOM(problem.data); 
+       let desc = dom.window.document.getElementsByClassName("problem_content");
+       CD console.log(desc[0].innerHTML);
+     
+   }); 
 }
 
 function dispatchToClients() { 
