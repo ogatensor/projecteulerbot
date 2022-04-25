d3843b7d2fdce607983016aee03c5326c2f4bee4
play.js <git@gituser.com>
2022-04-25 11:22:16 -0400

    fixed dependency and control flow issues

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ const axios = require('axios');
 const url = "https://projecteuler.net/problem="; 
 
 async function main() {
-     getDailyProblem(); 
+     await getDailyProblem(); 
      dispatchToClients(); 
 }
 
-function getDailyProblem() { 
+async function getDailyProblem() { 
    // choose a random problem number between the values or 1-700. 
    let number = Math.floor(Math.random() * 700); 
    
-   let problem = (number) => { 
-     // retrieve problem from ProjectEuler
-     let data = axios.get(`${url}/${number}.html`); 
-     data.then((problem) => { 
+// retrieve problem from ProjectEuler
+     console.log(`${url}${number}`);
+     const p = await axios.get(`${url}${number}`).then((problem) => { 
        console.log(problem); 
      }); 
- }  
- return problem(number); 
+console.log(p)
 }
 
 function dispatchToClients() { 
  return false; 
 }
-main(); 
+main()
