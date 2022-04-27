00b5efeaff6d50dbce6291dfa1b1cfd8098109db
tensor <drew.tensor@gmail.com>
2022-04-27 08:08:59 -0700

    add atcoder hook

---
+++ contest.js
@@ -0,0 +1,27 @@
+//pulls questioons from atcoder.jp contests 
+const axios = require('axios'); 
+const dom = require('jsdom'); 
+const { JSDOM } = dom; 
+const problems = ['a', 'b', 'c', 'd', 'e']; 
+
+async function AtCoderContestProblem() { 
+    // choose a random problem number between the values or 1-139. 
+    const contestNumber = Math.floor(Math.random() *  (139 - 1) + 1); 
+    const contestPath = `arc${contestNumber}_${problems[Math.floor(Math.random() * problems.length)]}`
+
+    const url = `https://atcoder.jp/contests/arc${contestNumber}/tasks/${contestPath}`; 
+
+    console.log(url)
+ // retrieve problem from ProjectEuler
+      console.log(`(@___@)/kimikoProblemBotv1:
+ [RunningTask] => Fetching AtCoder.jp Problem: # ${contestPath}`);
+    const p = await axios.get(`${url}`).then((problem) => { 
+        console.log(problem.data); 
+       // const dom = new JSDOM(problem.data); 
+       // let desc = dom.window.document.getElementsByClassName("problem_content");
+       // console.log(desc[0].innerHTML);
+      
+    }); 
+ }
+ AtCoderContestProblem(); 
+ module.AtCoderContestProblem = AtCoderContestProblem;
--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const axios = require('axios');
 const url = "https://projecteuler.net/problem="; 
 const dom = require('jsdom'); 
 const { JSDOM } = dom; 
+const { AtCoderContestProblem } = require("./contest.js"); 
 
 async function main() {
-     await getDailyProblem(); 
+  //   await getDailyProblem(); 
      dispatchToClients(); 
+     //get atcoder problem 
+     await AtCoderContestProblem;
 }
 
 async function getDailyProblem() { 
    // choose a random problem number between the values or 1-785. 
-   let number = Math.floor(Math.random() * 785); 
+   const number = Math.floor(Math.random() * 785); 
    
 // retrieve problem from ProjectEuler
      console.log(`(@___@)/kimikoProblemBotv1:
